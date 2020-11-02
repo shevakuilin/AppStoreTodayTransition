fd0640f02353f261f685650fab6c4e9518911e57
ShevaKuilin <shevakuilin@gmail.com>
2020-11-02 19:03:44 +0800

    调整 NestViewController 背景色

--- ASTTransition/ASTTransition/NestViewController.swift
+++ ASTTransition/ASTTransition/NestViewController.swift
@@ -10,17 +10,23 @@ import UIKit
 
 class NestViewController: UIViewController {
     public var headImage: UIImage?
+    private var textLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         print("进入NextVC")
         initElements()
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.textLabel.isHidden = false
+    }
 }
 
 private extension NestViewController {
     func initElements() {
-        self.view.backgroundColor = [255, 64, 63].color
+        self.view.backgroundColor = [245, 119, 159].color
         self.view.layer.masksToBounds = true
         self.view.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMinXMinYCorner]
         self.view.layer.cornerRadius = 20
@@ -33,6 +39,15 @@ private extension NestViewController {
         headImageView.contentMode = .center
 //        headImageView.frame = [0, 0, self.view.bounds.width, 500].frame
         self.view.addSubview(headImageView)
+        
+        textLabel = UILabel()
+        textLabel.font = UIFont.systemFont(ofSize: 20)
+        textLabel.textColor = .white
+        textLabel.text = "哔哩哔哩(゜-゜)つロ干杯~-bilibili"
+        textLabel.textAlignment = .center
+        textLabel.frame = [0, self.view.bounds.height - 100, self.view.bounds.width, 25].frame
+        self.view.addSubview(textLabel)
+        textLabel.isHidden = true
     }
 }
 
